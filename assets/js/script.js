3e1c1c511580f7cd10fdff76c6ba18ad06163ee8
Ana Domuta <anadomuta@Anas-MacBook-Pro.local>
2024-01-20 10:52:19 +0000

    added save button logic to save events in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
 $(document).ready(function () {
   var currentDay = $("#currentDay");
-  var currentTime = $("#currentTime");
+  var time = $("#hour");
+  var eventText = $(".description");
+  var saveButton = $(".saveBtn");
 
   currentDay.text(dayjs().format("dddd, MMMM Do"));
-  currentTime.text(dayjs().format("hh:mm A"));
+
+  // Save Event upon Button Click
+  function saveEvent() {
+    var eventFromLS = storeEvent();
+
+    var time = $(this).closest(".row").find(".currentTime").text();
+    var eventText = $(this).closest(".row").find(".description").val();
+
+    eventFromLS.push({ time: time, eventText: eventText });
+
+    localStorage.setItem("events", JSON.stringify(eventFromLS));
+  }
+
+  // Retrieve Events from Local Storage
+  function storeEvent() {
+    var eventFromLS = localStorage.getItem("events");
+    if (eventFromLS === null || eventFromLS === undefined) {
+      eventFromLS = [];
+      //   localStorage.setItem(".description", JSON.stringify(eventFromLS));
+    } else {
+      eventFromLS = JSON.parse(eventFromLS);
+    }
+    return eventFromLS;
+  }
+
+  saveButton.on("click", saveEvent);
 });
